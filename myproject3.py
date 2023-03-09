2f7375dcb307ee6bfdb328d60fc508a9d1d6e7f2
jeongik <kimjeongik1@naver.com>
2023-03-09 14:04:53 +0900

    곱하기 함수 추가 곱하기 수정

--- myproject3.py
+++ myproject3.py
@@ -1,9 +1,9 @@
 ### 함수
-def diff_func(n1, n2):
-    return n1 - n2
+def x_func(n1, n2):
+    return n1 * n2
 
 ### 전역
 num1, num2, res = 100, 200, 0
 ### 메인
-res = diff_func(num1, num2)
-print(num1, '-', num2, '=', res)
+res = x_func(num1, num2)
+print(num1, '*', num2, '=', res)
